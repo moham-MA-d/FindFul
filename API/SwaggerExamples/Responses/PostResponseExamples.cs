e6329d7f92de876eea90e045711754479cbe042f
mohammadAlvanpoor <mohammad.alvanpoor@gmail.com>
2022-06-07 12:43:20 +0430

    4

---
+++ API/SwaggerExamples/Responses/PostResponseExamples.cs
@@ -0,0 +1,19 @@
+﻿using DTO.Account;
+using DTO.Posts;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace API.SwaggerExamples.Responses
+{
+    public class PostResponseExamples : IExamplesProvider<DtoPostResponse>
+    {
+        public DtoPostResponse GetExamples()
+        {
+            var postResponse = new DtoPostResponse
+            {
+                Body = "This is a post response example.",
+                TheUser = new DtoMember()
+            };
+            return postResponse;
+        }
+    }
+}
