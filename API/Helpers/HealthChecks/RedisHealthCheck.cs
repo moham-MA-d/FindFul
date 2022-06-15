402647081f541f18198a557e9010eb11003911ad
mohammadAlvanpoor <mohammad.alvanpoor@gmail.com>
2022-06-15 20:05:34 +0430

    comment health check

--- API/Helpers/HealthChecks/RedisHealthCheck.cs
+++ API/Helpers/HealthChecks/RedisHealthCheck.cs
@@ -5,27 +5,27 @@ using System.Threading.Tasks;
 
 namespace API.Helpers.HealthChecks
 {
-    public class RedisHealthCheck : IHealthCheck
-    {
-        private readonly IConnectionMultiplexer _connectionMultiplexer;
+    //public class RedisHealthCheck : IHealthCheck
+    //{
+    //    private readonly IConnectionMultiplexer _connectionMultiplexer;
 
-        public RedisHealthCheck(IConnectionMultiplexer connectionMultiplexer)
-        {
-            _connectionMultiplexer = connectionMultiplexer;
-        }
+    //    public RedisHealthCheck(IConnectionMultiplexer connectionMultiplexer)
+    //    {
+    //        _connectionMultiplexer = connectionMultiplexer;
+    //    }
 
-        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                var db = _connectionMultiplexer.GetDatabase();
-                db.StringGet("a value that doesn't exist in Redis");
-                return await Task.FromResult(HealthCheckResult.Healthy());
-            }
-            catch (System.Exception ex)
-            {
-                return await Task.FromResult(HealthCheckResult.Unhealthy(ex.Message));
-            }   
-        }
-    }
+    //    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    //    {
+    //        try
+    //        {
+    //            var db = _connectionMultiplexer.GetDatabase();
+    //            db.StringGet("a value that doesn't exist in Redis");
+    //            return await Task.FromResult(HealthCheckResult.Healthy());
+    //        }
+    //        catch (System.Exception ex)
+    //        {
+    //            return await Task.FromResult(HealthCheckResult.Unhealthy(ex.Message));
+    //        }   
+    //    }
+    //}
 }
--- API/Services/Classes/RedisSubscriber.cs
+++ API/Services/Classes/RedisSubscriber.cs
@@ -5,23 +5,23 @@ using System.Threading.Tasks;
 
 namespace API.Services.Classes
 {
-    public class RedisSubscriber : BackgroundService
-    {
-        private readonly IConnectionMultiplexer _connectionMultiplexer;
+    //public class RedisSubscriber : BackgroundService
+    //{
+    //    private readonly IConnectionMultiplexer _connectionMultiplexer;
 
-        public RedisSubscriber(IConnectionMultiplexer connectionMultiplexer)
-        {
-            _connectionMultiplexer = connectionMultiplexer;
-        }
+    //    public RedisSubscriber(IConnectionMultiplexer connectionMultiplexer)
+    //    {
+    //        _connectionMultiplexer = connectionMultiplexer;
+    //    }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            var subscriber = _connectionMultiplexer.GetSubscriber();
+    //    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    //    {
+    //        var subscriber = _connectionMultiplexer.GetSubscriber();
 
-            return subscriber.SubscribeAsync("messages", (channel, value) =>
-            {
-                System.Console.WriteLine($"The message content is : {value}");
-            });
-        }
-    }
+    //        return subscriber.SubscribeAsync("messages", (channel, value) =>
+    //        {
+    //            System.Console.WriteLine($"The message content is : {value}");
+    //        });
+    //    }
+    //}
 }
