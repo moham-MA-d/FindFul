c1237cfd11fdec9b7e98fa8284777b85d004fe22
moham-MA-d <alvanpour.mohammad.1990@gmail.com>
2023-12-10 17:58:07 +0330

    4

--- API/Extensions/CommonExtensions.cs
+++ API/Extensions/CommonExtensions.cs
@@ -9,6 +9,7 @@ namespace API.Extensions
         public static string GetCurrentLocationUri(this HttpContext httpContext)
         {
             var baseUrl = $"{httpContext.Request.Scheme}://{httpContext.Request.Host.ToUriComponent()}{httpContext.Request.Path}";
+
             var index = baseUrl.LastIndexOf("/", StringComparison.Ordinal);
             if (index >= 0)
                 baseUrl = baseUrl[..index]; // or index + 1 to keep slash
